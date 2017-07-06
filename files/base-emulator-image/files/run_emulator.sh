93f03feef762eb9b49c419877fe2ff615c5034ad
dolf <dolf@yandex-team.ru>
2017-07-06 18:42:32 +0300

    selenoid start

--- files/base-emulator-image/files/run_emulator.sh
+++ files/base-emulator-image/files/run_emulator.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-/opt/x.sh
+function run_x_server () {
+    nohup X :10 &
+    nohup x11vnc -loop -display :10 &
+}
+
+function run_emulator () {
+    nohup /opt/android-sdk/tools/emulator64-x86 -avd Emulator -no-audio -no-boot-anim -no-jni -gpu mesa -qemu -enable-kvm &
+}
+
+function wait_for_emulator () {
+    until [[ "$bootanim" =~ "stopped" ]]; do
+        bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+        sleep 1
+    done
+}
+
+run_x_server
 sleep 5
-nohup /opt/android-sdk/tools/emulator64-x86 -avd Emulator -no-audio -no-boot-anim -no-jni -gpu mesa -qemu -enable-kvm &
+run_emulator
+wait_for_emulator
 
 exit 0
--- files/base-emulator-image/files/x.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nohup X :10 &
-nohup x11vnc -loop -display :10 &
-
-exit 0
